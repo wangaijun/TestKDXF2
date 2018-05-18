9be5053d9960e76dbdeff65ea971ed7640bb47ae
waj <wangaj@glodon.com>
2018-05-18 11:07:24 +0800

    删除多余的文件

--- speechDemo/src/main/java/com/iflytek/speech/setting/SpeechRecognizeSettings.kt
+++ speechDemo/src/main/java/com/iflytek/speech/setting/SpeechRecognizeSettings.kt
@@ -1,13 +1,10 @@
 package com.iflytek.speech.setting
 
 import android.os.Bundle
-import android.preference.EditTextPreference
 import android.preference.Preference
 import android.preference.Preference.OnPreferenceChangeListener
 import android.preference.PreferenceActivity
 import android.view.Window
-
-import com.iflytek.speech.util.SettingTextWatcher
 import com.iflytek.voicedemo.R
 
 /**
