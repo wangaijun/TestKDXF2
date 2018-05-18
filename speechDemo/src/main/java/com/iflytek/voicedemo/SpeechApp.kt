d9d851e137b2a285db5dad871ee0dd53aeb75b5a
waj <wangaj@glodon.com>
2018-05-18 10:30:08 +0800

    将app转换成kotlin代码

--- speechDemo/src/main/java/com/iflytek/voicedemo/SpeechApp.kt
+++ speechDemo/src/main/java/com/iflytek/voicedemo/SpeechApp.kt
@@ -15,7 +15,7 @@ class SpeechApp : Application() {
 
         // 注意： appid 必须和下载的SDK保持一致，否则会出现10407错误
 
-        SpeechUtility.createUtility(this@SpeechApp, "appid=" + getString(R.string.app_id))
+        SpeechUtility.createUtility(this, "appid=" + getString(R.string.app_id))
 
         // 以下语句用于设置日志开关（默认开启），设置成false时关闭语音云SDK日志打印
         // Setting.setShowLog(false);
