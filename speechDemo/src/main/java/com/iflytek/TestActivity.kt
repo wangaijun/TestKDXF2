c17b17cb5d0a652ef2edcabdfc75089a7b7ee3ea
waj <wangaj@glodon.com>
2018-05-18 13:46:20 +0800

    添加调用窗体

--- speechDemo/src/main/java/com/iflytek/TestActivity.kt
+++ speechDemo/src/main/java/com/iflytek/TestActivity.kt
@@ -1,13 +1,23 @@
 package com.iflytek
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import com.iflytek.voicedemo.R
+import com.iflytek.voicedemo.SpeechRecognizeActivity
+import kotlinx.android.synthetic.main.activity_test.*
 
 class TestActivity : Activity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
+
+        btn1.setOnClickListener{
+            startActivityForResult(Intent(this,SpeechRecognizeActivity::class.java), REQUEST_VOICE_INPUT)
+        }
+    }
+
+    companion object {
+        const val REQUEST_VOICE_INPUT = 100
     }
 }
