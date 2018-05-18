39988b59e0e14140075e11949e68e7689a13e796
waj <wangaj@glodon.com>
2018-05-18 13:36:28 +0800

    为封装做准备

---
+++ speechDemo/src/main/java/com/iflytek/TestActivity.kt
@@ -0,0 +1,13 @@
+package com.iflytek
+
+import android.app.Activity
+import android.os.Bundle
+import com.iflytek.voicedemo.R
+
+class TestActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+    }
+}
