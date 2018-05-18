b3980b7fdfe2312a58990216ecfa04d2c1c42f68
waj <wangaj@glodon.com>
2018-05-18 08:41:24 +0800

    删除多余的设置项

--- speechDemo/src/main/java/com/iflytek/speech/setting/SpeechRecognizeSettings.java
+++ speechDemo/src/main/java/com/iflytek/speech/setting/SpeechRecognizeSettings.java
@@ -16,21 +16,13 @@ import com.iflytek.voicedemo.R;
 public class SpeechRecognizeSettings extends PreferenceActivity implements OnPreferenceChangeListener {
 	
 	public static final String PREFER_NAME = "com.iflytek.setting";
-	private EditTextPreference mVadbosPreference;
-	private EditTextPreference mVadeosPreference;
-	
+
 	@SuppressWarnings("deprecation")
 	public void onCreate(Bundle savedInstanceState) {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		super.onCreate(savedInstanceState);
 		getPreferenceManager().setSharedPreferencesName(PREFER_NAME);
-		addPreferencesFromResource(R.xml.iat_setting);
-		
-		mVadbosPreference = (EditTextPreference)findPreference("iat_vadbos_preference");
-		mVadbosPreference.getEditText().addTextChangedListener(new SettingTextWatcher(SpeechRecognizeSettings.this,mVadbosPreference,0,10000));
-		
-		mVadeosPreference = (EditTextPreference)findPreference("iat_vadeos_preference");
-		mVadeosPreference.getEditText().addTextChangedListener(new SettingTextWatcher(SpeechRecognizeSettings.this,mVadeosPreference,0,10000));
+		addPreferencesFromResource(R.xml.speech_recognize_setting);
 	}
 	@Override
 	public boolean onPreferenceChange(Preference preference, Object newValue) {
