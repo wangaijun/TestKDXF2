10754741de1f27278bf0ce86207cbdc874545237
waj <wangaj@glodon.com>
2018-05-21 10:19:03 +0800

    绘制声音数据

--- app/src/main/java/com/example/waj/testkdxf2/MyView.kt
+++ app/src/main/java/com/example/waj/testkdxf2/MyView.kt
@@ -2,6 +2,8 @@ package com.example.waj.testkdxf2
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 
@@ -9,12 +11,25 @@ import android.view.View
  * Created by waj on 18-5-18.
  */
 class MyView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
-    val pip:MyPip = MyPip(10)
+    val pip:MyPip = MyPip(size)
+    val paint:Paint = Paint()
+    init {
+        paint.color = Color.RED
+    }
     override fun onDraw(canvas: Canvas?) {
         if (canvas==null) return
         val c:Canvas = canvas
-
+        val cw = width/size
+        val i = 0
+        var x = 0
+        while (i<size){
+            x += cw / 2
+            val y = pip.getDatas()[i]
+            c.drawCircle(x.toFloat(),y.toFloat(),5.toFloat(),paint)
+        }
     }
 
-
+    companion object {
+        const val size = 10
+    }
 }
