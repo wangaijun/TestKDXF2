da39bbb1b4fd059b7ecf23fe6baba9460bd01710
waj <wangaj@glodon.com>
2018-05-21 13:52:02 +0800

    显示声音的波动

--- speechDemo/src/main/java/com/iflytek/MyView.kt
+++ speechDemo/src/main/java/com/iflytek/MyView.kt
@@ -13,24 +13,50 @@ import android.view.View
 class MyView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
     val pip: MyPip = MyPip(size)
     val paint:Paint = Paint()
+    lateinit var t:MyThread
     init {
         paint.color = Color.RED
     }
+
     override fun onDraw(canvas: Canvas?) {
         if (canvas==null) return
         val c:Canvas = canvas
-        val cw = width/ size
+        val cw = width/size
         var i = 0
-        var x = 0
+        var x = cw/2
         while (i< size){
-            x += cw / 2
+            x += cw
             val y = pip.getDatas()[i]
             c.drawCircle(x.toFloat(),y.toFloat(),5.toFloat(),paint)
             i++
+            println("waj:$x,$y")
         }
     }
 
     companion object {
-        const val size = 10
+        const val size = 20
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        t = MyThread()
+        t.start()
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        if (t!=null){
+            t.loop = false
+        }
+    }
+
+    inner class MyThread:Thread(){
+        var loop = true
+        override fun run() {
+            while (loop){
+                Thread.sleep(16)
+                postInvalidate()
+            }
+        }
     }
 }
