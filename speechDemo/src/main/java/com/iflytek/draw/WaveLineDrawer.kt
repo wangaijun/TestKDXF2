8cfe1618fbd026d6a8afed8daceecf661d028bfe
waj <wangaj@glodon.com>
2018-05-22 09:59:59 +0800

    绘制波浪线

--- speechDemo/src/main/java/com/iflytek/draw/WaveLineDrawer.kt
+++ speechDemo/src/main/java/com/iflytek/draw/WaveLineDrawer.kt
@@ -2,6 +2,7 @@ package com.iflytek.draw
 
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.Path
 import com.iflytek.MyPip
 import com.iflytek.VoiceEffectView
 
@@ -9,34 +10,51 @@ import com.iflytek.VoiceEffectView
  * Created by waj on 18-5-22.
  */
 class WaveLineDrawer: Drawer(){
+    val path = Path()
     override fun draw(c: Canvas, width: Float, height: Float, pip: MyPip, paint: Paint) {
         val size = VoiceEffectView.size
+        path.reset()
+        val y = height/2
+        var i=0
+        var x=0F
         val cw = width/size
-        var i = 0
-        var x = cw/2
-        val arr = FloatArray(size*4-1)
-        while (i< size){
-            x += cw
-            val y = pip.getDatas()[i]*3F
-            if (i==0) {
-                arr[4*i] = x
-                arr[4*i + 1] = y
-            }
-            else if (i== size-1){
-                arr[4*(i-1) + 2] = x
-                arr[4*(i-1) + 3] = y
-            }
-            else{
-                arr[4*(i-1) + 2] = x
-                arr[4*(i-1) + 3] = y
-                arr[4*i] = x
-                arr[4*i + 1] = y
-            }
-
+        path.moveTo(-cw,y)
+        val datas = pip.getDatas()
+        while (i<size/2){
+            x += i*2*cw
+            path.rQuadTo(x, datas[i]*3F,x+cw, datas[i+1]*3F)
             i++
-            println("waj:$x,$y")
         }
-        c.drawLines(arr,paint)
+        c.drawPath(path,paint)
     }
 
+    /**
+     * val size = VoiceEffectView.size
+    val cw = width/size
+    var i = 0
+    var x = cw/2
+    val arr = FloatArray(size*4-1)
+    while (i< size){
+    x += cw
+    val y = pip.getDatas()[i]*3F
+    if (i==0) {
+    arr[4*i] = x
+    arr[4*i + 1] = y
+    }
+    else if (i== size-1){
+    arr[4*(i-1) + 2] = x
+    arr[4*(i-1) + 3] = y
+    }
+    else{
+    arr[4*(i-1) + 2] = x
+    arr[4*(i-1) + 3] = y
+    arr[4*i] = x
+    arr[4*i + 1] = y
+    }
+
+    i++
+    println("waj:$x,$y")
+    }
+    c.drawLines(arr,paint)
+     * */
 }
