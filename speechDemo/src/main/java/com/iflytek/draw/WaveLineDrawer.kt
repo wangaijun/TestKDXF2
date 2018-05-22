4f736b6ba38abb9843c2be04cacf6f33f6315f39
waj <wangaj@glodon.com>
2018-05-22 15:47:43 +0800

    基本上能正常画波浪线了

--- speechDemo/src/main/java/com/iflytek/draw/WaveLineDrawer.kt
+++ speechDemo/src/main/java/com/iflytek/draw/WaveLineDrawer.kt
@@ -11,64 +11,35 @@ import com.iflytek.VoiceEffectView
  */
 class WaveLineDrawer: Drawer(){
     val path = Path()
-
     override fun draw(c: Canvas, width: Float, height: Float, pip: MyPip, paint: Paint) {
-        val size = VoiceEffectView.size
         val y = height/2
-        c.translate(0F,y)
-        var i=0
-        var x=0F
-        val cw = width/size
         path.reset()
-        path.moveTo(-cw,0f)
-        val datas = arrayListOf<Int>()
-        pip.getDatas().forEach { datas.add(it) }  //估计10为中值,从10分开,分成两部分
-        var v2 = 0f
-        while (i<size){
-            x += i*cw
-            val v1 = getNextSign(v2) * v2
-            v2 = getNextSign(v1)*datas[i] * 2F
-            path.quadTo(x, v1,x+cw/2, v2)
-            path.moveTo(x+cw/2, v2)
-            i++
-        }
         paint.style = Paint.Style.STROKE
-        paint.strokeWidth = 3.toFloat()
-//        paint.isAntiAlias = true
-        c.drawPath(path,paint)
+        paint.strokeWidth = 5.toFloat()
+        paint.isAntiAlias = true
+        val v = pip.getDatas().last().toFloat()
+        var l = y-3*v
+        var m = y
+        var h = y+3*v
+        val n = 6
+        c.translate(-index.toFloat(),0F)
+        path.moveTo(0F, m)
+        path.quadTo(width/n, l, width*2/n, m)
+        path.moveTo(width*2/n, m)
+        path.quadTo(width*3/n, h, width*4/n, m)
+        path.moveTo(width*4/n, m)
+        path.quadTo(width*5/n, l, width*6/n, m)
+        path.moveTo(width*6/n, m)
+        path.quadTo(width*7/n, h, width*8/n, m)
+        path.moveTo(width*8/n, m)
+        path.quadTo(width*9/n, l, width*10/n, m)
+        index += 1
+        if (index>=width*4/6) index = 0
+        println("waj:$index")
+        c.drawPath(path, paint)
     }
 
-    private fun getNextSign(v: Float): Int {
-        return if (v>=0) -1 else 1
+    companion object {
+        var index = 0
     }
-
-    /**
-     * val size = VoiceEffectView.size
-    val cw = width/size
-    var i = 0
-    var x = cw/2
-    val arr = FloatArray(size*4-1)
-    while (i< size){
-    x += cw
-    val y = pip.getDatas()[i]*2F
-    if (i==0) {
-    arr[4*i] = x
-    arr[4*i + 1] = y
-    }
-    else if (i== size-1){
-    arr[4*(i-1) + 2] = x
-    arr[4*(i-1) + 3] = y
-    }
-    else{
-    arr[4*(i-1) + 2] = x
-    arr[4*(i-1) + 3] = y
-    arr[4*i] = x
-    arr[4*i + 1] = y
-    }
-
-    i++
-    println("waj:$x,$y")
-    }
-    c.drawLines(arr,paint)
-     * */
 }
