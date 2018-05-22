cb67b96eb16b65df30fb875dcd50b5097a330a30
waj <hw_wangaijun#163.com>
2018-05-22 22:53:12 +0800

    防止数据越界

--- speechDemo/src/main/java/com/iflytek/widget/draw/WaveDrawer.kt
+++ speechDemo/src/main/java/com/iflytek/widget/draw/WaveDrawer.kt
@@ -13,7 +13,7 @@ class WaveDrawer: Drawer(){
         val cw = width*1.0F/ VoiceEffectView.size
         var i = 0
         var x = 0F
-        while (i< VoiceEffectView.size){
+        while (i< pip.getDatas().size){
             x += cw
             val y = height/2F
             paint.strokeWidth = pip.getDatas()[i].toFloat()*3
