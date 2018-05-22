d03305cdc4df32040d37a6404b89103d3df7c8ed
waj <wangaj@glodon.com>
2018-05-22 08:57:23 +0800

    添加画波浪线的策略实现类

---
+++ speechDemo/src/main/java/com/iflytek/draw/WaveLineDrawer.kt
@@ -0,0 +1,15 @@
+package com.iflytek.draw
+
+import android.graphics.Canvas
+import android.graphics.Paint
+import com.iflytek.MyPip
+
+/**
+ * Created by waj on 18-5-22.
+ */
+class WaveLineDrawer: Drawer(){
+    override fun draw(c: Canvas, width: Float, height: Float, pip: MyPip, paint: Paint) {
+
+    }
+
+}
